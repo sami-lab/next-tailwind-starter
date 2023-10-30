c933b3aa18e639a5b57611ecfccbe71fc61426dd
Muhammad Sami <56019715+sami-lab@users.noreply.github.com>
2023-10-30 06:50:42 +0500

    first commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  compiler: {
+    removeConsole: true,
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/app/page.js
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <main className='flex flex-col items-center justify-center'>Test</main>
+  );
+}
