f5e7fe4287241faf317eb886f5427ab53e9120ad
Muhammad Sami <56019715+sami-lab@users.noreply.github.com>
2023-10-31 07:40:50 +0500

    grid

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-    <main className='flex flex-col items-center justify-center'>Test</main>
+    <div className='grid grid-cols-12 gap-4'>
+      <div className='col-span-2  border border-solid border-light'></div>
+      <div className='col-span-7  border border-solid border-red'></div>
+      <div className='col-span-3  border border-solid border-light'></div>
+    </div>
   );
 }
